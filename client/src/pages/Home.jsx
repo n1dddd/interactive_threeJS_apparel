0733b664d9bce8dbefa7aa31c760b452b3461644
Daniel <danielseniv@outlook.com>
2023-07-10 17:28:35 -0400

    Add clarifying comments

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 import { CustomButton } from '../components';
 
-import state from '../store'
+import state from '../store' //import state managed by valtio
 
 import {
   headContainerAnimation,
@@ -42,13 +42,11 @@ const Home = () => {
               </p>
 
               <CustomButton //passing props to CustomButton component
-              type="filled"
+              type="filled" //pass what state the button should be in terms of styling
               title="Customize it"
               handleClick={() => state.intro = false} //changes intro state to false, text transitions away
               customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-              >
-
-              </CustomButton>
+              />
             </motion.div>
           </motion.div>
         </motion.section>
