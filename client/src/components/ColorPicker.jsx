7c59505c41d485bf2066b1317889e225b7f91e52
Daniel <danielseniv@outlook.com>
2023-07-10 21:34:41 -0400

    Use sketchpicker from react-color to add color picker to ColorPicker component

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../store'
 
 const ColorPicker = () => {
+
+  const snap = useSnapshot(state);
+
   return (
-    <div>ColorPicker</div>
+    <div className="absolute left-full ml-3">
+      <SketchPicker 
+        color={snap.color} //display the color being held in state
+        disableAlpha
+        onChange={(color) => state.color = color.hex} //from the sketchpicker, state variable color, is set to color from sketchpicker
+      />
+    </div>
   )
 }
 
