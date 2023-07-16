1704fb17e661949518763b8c6e7ba6deb2bcec16
Daniel <danielseniv@outlook.com>
2023-07-16 12:25:49 -0400

    Create AI picker component to generate logo and full t-shirt background

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 
-const AIPicker = () => {
+import CustomButton from './CustomButton';
+
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit}) => {
   return (
-    <div>AIPicker</div>
+    <div className="aipicker-container">
+      <textarea
+      placeholder="Ask AI..."
+      className="aipicker-text-area"
+      rows={5}
+      value={prompt}
+      onChange={(e) => setPrompt(e.target.value)}
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton 
+            type="outline"
+            title="Asking AI..."
+            customStyles="text-xs"
+          />
+        ) : (
+        <>
+          <CustomButton
+            type="outline"
+            title="AI logo"
+            handleClick={() => handleSubmit('logo')}
+            customStlyes="text-xs"
+          />
+          <CustomButton
+            type="filled"
+            title="AI Full"
+            handleClick={() => handleSubmit('full')}
+            customStlyes="text-xs"
+          />
+        </> )}
+      </div>
+    </div>
   )
 }
 
